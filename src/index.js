e63aad77d02e79577b2e2362005ddac1a43f482e
utkarshsaraf <utkarsh.sf90@gmail.com>
2025-04-10 10:45:17 +0530

    changes in index.js

--- src/index.js
+++ src/index.js
@@ -2,16 +2,7 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 
 function Greeting() {
-  return (
-    <div>
-      <ul>
-        <li>Item 1</li>
-        <li>Item 2</li>
-        <li>Item 3</li>
-      </ul>
-      <h2>My First Component</h2>
-    </div>
-  )
+  return <input type="text" name="" id="" placeholder="Enter your name" />
 }
 
 // function Greeting() {
